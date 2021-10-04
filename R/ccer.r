c82e3bbd622c665d3d4102e1ae65a3a057685ba1
Han Zhang <zhanghan426@gmail.com>
2021-10-04 16:01:44 +0800

    add bootstrap for LPM (linear probability model)

--- R/ccer.r
+++ R/ccer.r
@@ -68,7 +68,17 @@ cdf_logit= function(mX, vBeta) {
   return(1/(1+ exp(-1 * (mX %*% vBeta))) )
 }
 
-# Likelihood and MLE for ML classifications as independent variables (linear regression) -----------------
+# ytr = rnorm(n + m, mean = b0 + b1 * x + b2 * w + eps, sd = 1)
+complement <- function(y, rho, x) {
+  if (missing(x)) x <- rnorm(length(y)) # Optional: supply a default if `x` is not given
+  y.perp <- residuals(lm(x ~ y))
+  rho * sd(y.perp) * y + y.perp * sd(y) * sqrt(1 - rho^2)
+}
+
+
+
+# MLE --------------
+## MLE for ML classifications as independent variables (linear regression) -----------------
 linear_density <- function(vY, mX, vBeta){
   return (dnorm(vY - mX %*% vBeta ))
 }
@@ -129,7 +139,7 @@ MLE_linear <- function(formula, data, predicted_col_name, ppv = 0, npv = 0) {
   return (d)
 }
 
-# Likelihood and MLE for ML classification as outcomes (logistic regression) -----------------
+## MLE for ML classification as outcomes (logistic regression) -----------------
 logLikelihoodLogit = function(vBeta, mX, vY, fnr = 0, fpr = 0) {
   return(-sum(
     vY * log( fpr + (1 - fnr - fpr) * logit(mX, vBeta) ) +
@@ -163,8 +173,7 @@ MLE_logit <- function(formula, data, fnr = 0, fpr = 0) {
   # optimize
   optimModel <- optim(vBetfnr, logLikelihoodLogit,
                      mX = mX, vY = vY, fnr = fnr, fpr = fpr,
-                     method = 'L-BFGS-B',## does not work when FNR and FPR are both high
-                     # method = 'BFGS', ## does not work when FPR is high and FPR is low; or vice versa
+                     method = 'L-BFGS-B',
                      lower = rep(inff, ncol(mX)), upper = rep(uppf, ncol(mX)),
 
                      hessian=TRUE,  control = list(maxit =300))
@@ -181,7 +190,7 @@ MLE_logit <- function(formula, data, fnr = 0, fpr = 0) {
 
 
 
-# Likelihood and MLE for ML classification as outcome (linear regression or linear prob model or LPM) -----------------
+## MLE for ML classification as outcome (linear regression or linear prob model or LPM) -----------------
 
 logLikelihood_LPM = function(vBeta, mX, vY, fnr = 0, fpr = 0) {
 
@@ -235,7 +244,7 @@ MLE_LPM <- function(formula, data,  fnr = 0, fpr = 0) {
   return (d)
 }
 
-# bootstrap ---------------------
+# Bootstrap ---------------------
 
 ## bootstrap for logistic regresion (outcome case)------
 
@@ -249,7 +258,7 @@ MLE_LPM <- function(formula, data,  fnr = 0, fpr = 0) {
 #' @param pl vector of predicted values; needs to be of the same dimension of the vevtor of true values.
 #' @param nboot number of bootstrap samples
 #' @return_boot_samples whether to return raw bootstrap estimates
-#' @return if return_boot_samples is false, return mean/standard error/quantile statistics from bootstrap sample only; otherwise return a list, the first element of which is mean/standard error/quantile statistics from bootstrap sample , and the second ele,ent of which is `nboot` samples from bootstrap draws
+#' @return if return_boot_samples is false, return mean/standard error/quantile statistics from bootstrap sample only; otherwise return a list, the first element of which is a dataframe of `nboot` rows, with each row as one estimated coefficients from a bootstrap resample, and the second element of which a dataframe of `nboot` rows, with each row as one pair  of estimated error rates: FPR and FNR. Powerful users can set `return_boot_samples` to be true to calculate the confidence interval of estimates by their own.
 #' @export
 MLE_logit_bootstrap <- function(formula, data, tl , pl, nboot = 10000, return_boot_samples = FALSE) {
 
@@ -285,7 +294,7 @@ MLE_logit_bootstrap <- function(formula, data, tl , pl, nboot = 10000, return_bo
                         mX = mX, vY = vY, fnr = fnr, fpr = fpr,
                         # method = 'L-BFGS-B',
                         # method = 'BFGS',
-                        method = 'L-BFGS-B',## does not work when FNR and FPR are both high
+                        method = 'L-BFGS-B',
                         lower = rep(inff, ncol(mX)), upper = rep(uppf, ncol(mX)),
                         hessian=TRUE,  control = list(maxit = 300))
     coef = optimModel$par
@@ -304,20 +313,85 @@ MLE_logit_bootstrap <- function(formula, data, tl , pl, nboot = 10000, return_bo
   fnr.sd <- sd(err[,1])
   fpr.sd <- sd(err[,2])
 
-  # tv  <- est/sdd
-  # pv <- 2 * pt(tv, df = nrow(mX) - ncol(mX), lower.tail = F) ## two-tailed
-
-  # d = data.frame("Estimate" = t(est),  "Std. Error" = t(sdd), "z value" = t(tv),  "Pr(>|z|)" = t(pv), check.names = FALSE)
-  # d = data.frame("Estimate" = est,  "Std. Error" = sdd, "z value" = tv,  "Pr(>|z|)" = pv, check.names = FALSE)
-  d = data.frame("Estimate" = est,  "Std. Error" = sdd, check.names = FALSE)
-  d = rbind(d, data.frame("Estimate" = c(fnr, fpr),  "Std. Error" = c(fnr.sd, fpr.sd), check.names = FALSE))
+  d0 = data.frame("Estimate" = est,  "Std. Error" = sdd, check.names = FALSE)
+  d = rbind(d0, data.frame("Estimate" = c(fnr, fpr),  "Std. Error" = c(fnr.sd, fpr.sd), check.names = FALSE))
 
   # return coefficient estimates as well as raw res
   if (return_boot_samples ){
-    return (list(bayesian = d, raw_error = err))
+    return (list(resamples = res, raw_error = err))
     }else{
   return (d)
     }
+}
+
+## bootstrap for linear probability model (outcome case)------
+
+#' Bootstrap estimator for linear regression with binary classification as *outcome* variable
+#'
+#' This functions takes binary classification as *outcome* variables, apply MLE estimator, and returned corrected coefficient estimates.
+#'
+#' @param formula R formula, such as y ~ x
+#' @param data a data.frame object
+#' @param tl vector of true values
+#' @param pl vector of predicted values; needs to be of the same dimension of the vevtor of true values.
+#' @param nboot number of bootstrap samples
+#' @return_boot_samples whether to return raw bootstrap estimates
+#' @return if return_boot_samples is false, return mean/standard error/quantile statistics from bootstrap sample only; otherwise return a list, the first element of which is a dataframe of `nboot` rows, with each row as one estimated coefficients from a bootstrap resample, and the second element of which a dataframe of `nboot` rows, with each row as one pair  of estimated error rates: FPR and FNR. Powerful users can set `return_boot_samples` to be true to calculate the confidence interval of estimates by their own.
+#' @export
+MLE_LPM_bootstrap <- function(formula, data, tl , pl, nboot = 10000, return_boot_samples = FALSE) {
+
+  initModel = lm(formula,
+                 data = data)
+  mX = model.matrix(initModel)
+  vY = model.frame(initModel)[,1]
+  vBetfnr = coef(initModel) + rnorm(ncol(mX), 0, 0.05)
+  ss = coef(summary(initModel))[, 2]
+  inff = coef(initModel) - 10*ss
+  uppf = coef(initModel) + 10*ss
+
+  res <- c()
+  err <- c()
+  for (i in 1:nboot){
+
+    idx <- dqsample.int(length(pl), length(pl), replace = T)
+    pl_boot <- pl[idx]
+    tl_boot <- tl[idx]
+
+
+    fnr <- FNR (tl_boot, pl_boot)
+    fpr <- FPR (tl_boot, pl_boot)
+
+    optimModel <- optim(vBetfnr, logLikelihood_LPM,
+                        mX = mX, vY = vY, fnr = fnr, fpr = fpr,
+                        # method = 'BFGS',
+                        method = 'L-BFGS-B',
+                        lower = rep(inff, ncol(mX)), upper = rep(uppf, ncol(mX)),
+                        hessian=TRUE,  control = list(maxit = 300))
+    coef = optimModel$par
+    coef.sd = sqrt(diag(solve( optimModel$hessian)))
+    # res <- rbind(res, data.frame("Estimate" = coef,  "Std. Error" = coef.sd))
+    res <- rbind(res, t(coef))
+    err <- rbind(err, c(fnr, fpr))
+  }
+  # construct output
+
+  est <- colMeans(res)
+  sdd <- apply(res, 2, sd)
+
+  fnr <- mean(err[,1])
+  fpr <- mean(err[,2])
+  fnr.sd <- sd(err[,1])
+  fpr.sd <- sd(err[,2])
+
+  d0 = data.frame("Estimate" = est,  "Std. Error" = sdd, check.names = FALSE)
+  d = rbind(d0, data.frame("Estimate" = c(fnr, fpr),  "Std. Error" = c(fnr.sd, fpr.sd), check.names = FALSE))
+
+  # return coefficient estimates as well as raw res
+  if (return_boot_samples ){
+    return (list(resamples = res, raw_error = err))
+  }else{
+    return (d)
+  }
 }
 
 
@@ -335,7 +409,7 @@ MLE_logit_bootstrap <- function(formula, data, tl , pl, nboot = 10000, return_bo
 #' @param pl vector of predicted values; needs to be of the same dimension of the vevtor of true values.
 #' @param nboot number of bootstrap samples
 #' @return_boot_samples whether to return raw bootstrap estimates
-#' @return if return_boot_samples is false, return mean/standard error/quantile statistics from bootstrap sample only; otherwise return a list, the first element of which is mean/standard error/quantile statistics from bootstrap sample , and the second ele,ent of which is `nboot` samples from bootstrap draws
+#' @return if return_boot_samples is false, return mean/standard error/quantile statistics from bootstrap sample only; otherwise return a list, the first element of which is a dataframe of `nboot` rows, with each row as one estimated coefficients from a bootstrap resample, and the second element of which a dataframe of `nboot` rows, with each row as one pair  of estimated error rates: PPV and NPV. Powerful users can set `return_boot_samples` to be true to calculate the confidence interval of estimates by their own.
 #' @export
 MLE_linear_indep_bootstrap <- function(formula, data,predicted_col_name, tl , pl, nboot = 10000, return_boot_samples = FALSE) {
 
@@ -351,6 +425,7 @@ MLE_linear_indep_bootstrap <- function(formula, data,predicted_col_name, tl , pl
 
   res <- c()
   err <- c()
+  d <- c()
   for (i in 1:nboot){
 
     idx <- dqrng::dqsample.int(length(pl), length(pl), replace = T)
@@ -385,26 +460,20 @@ MLE_linear_indep_bootstrap <- function(formula, data,predicted_col_name, tl , pl
   fpr.sd <- sd(err[,2])
 
 
-  d = data.frame("Estimate" = est,  "Std. Error" = sdd, check.names = FALSE)
-  d = rbind(d, data.frame("Estimate" = c(fnr, fpr),  "Std. Error" = c(fnr.sd, fpr.sd), check.names = FALSE))
+  d0 = data.frame("Estimate" = est,  "Std. Error" = sdd, check.names = FALSE)
+  d = rbind(d0, data.frame("Estimate" = c(fnr, fpr),  "Std. Error" = c(fnr.sd, fpr.sd), check.names = FALSE))
   # return coefficient estimates as well as raw res
   if (return_boot_samples ){
-    return (list(bayesian = d, raw_error = err))
+    return (list(resamples = d, raw_error = err))
     }else{
   return (d)
     }
 }
 
 
-# ytr = rnorm(n + m, mean = b0 + b1 * x + b2 * w + eps, sd = 1)
-complement <- function(y, rho, x) {
-  if (missing(x)) x <- rnorm(length(y)) # Optional: supply a default if `x` is not given
-  y.perp <- residuals(lm(x ~ y))
-  rho * sd(y.perp) * y + y.perp * sd(y) * sqrt(1 - rho^2)
-}
 
-
-# Bayesian logistic regression -------------
+# Bayesian ------------
+## Bayesian logistic regression -------------
 
 #' Bayesian estimator for logistic regression with binary classification as *outcome* variable
 #'
@@ -419,7 +488,7 @@ complement <- function(y, rho, x) {
 #' @param tp number of true positives in validation data
 #' @param tn number of true negatives in validation data
 #' @param tau_prior prior values of hyperparameters of Bayesian regression variance
-#' @param working_dir directory to store JAGS temporary files
+#' @param working_dir directory to store JAGS temporary files; default is the same directory of the script
 #' @param n.iter number of Bayesian draws
 #' @return a dataframe contining estimated values, standard errors, z values and p values
 #' @export
@@ -449,7 +518,7 @@ bayesian_logistic <- function(formula, data, fn = fn, fp = fp, tp = tp, tn = tn,
       logit(p[i]) <- inprod(x[i,], beta[])
     }
 
-    # hieaarchical prior -----------
+    # hieaarchical prior
     # beta[1:num_param] ~ dmnorm(mea[], preci[,])
     # for( k in 1:num_param){
     #   # mea[k] ~ dnorm(0, 0.0001)
@@ -462,14 +531,14 @@ bayesian_logistic <- function(formula, data, fn = fn, fp = fp, tp = tp, tn = tn,
     # }
     # tau ~ dgamma(tau_prior, tau_prior)
 
-    # standard logistic  -----------
+    # standard logistic
     for( l in 1:num_param){
       beta[l] ~ dnorm(0, tau)
     }
 
     tau ~ dgamma(tau_prior, tau_prior)
 
-    # fpr/fnr estimate ------
+    # fpr/fnr estimate
     lambda[1] ~ dbeta(fn , tp ) ## FNR
     lambda[2] ~ dbeta(fp , tn ) ## FPR
 
@@ -534,7 +603,7 @@ bayesian_logistic <- function(formula, data, fn = fn, fp = fp, tp = tp, tn = tn,
 
 
 
-# Bayesian linear regression for predicted independent variable ----------------
+## Bayesian linear regression for predicted independent variable ----------------
 
 #' Bayesian estimator for linear with binary classification as *independent* variable
 #'
@@ -550,7 +619,7 @@ bayesian_logistic <- function(formula, data, fn = fn, fp = fp, tp = tp, tn = tn,
 #' @param tn number of true negatives in validation data
 #' @param predicted_col_name string or integer indicating the column of the variable that is predicted
 #' @param tau_prior prior values of hyperparameters of Bayesian regression variance
-#' @param working_dir directory to store JAGS temporary files
+#' @param working_dir directory to store JAGS temporary files; default is the same directory of the script
 #' @param n.iter number of Bayesian draws
 #' @return a dataframe contining estimated values, standard errors, z values and p values
 #' @export
@@ -659,4 +728,122 @@ bayesian_linear_indep <- function(formula, data, fn = fn, fp = fp, tp = tp, tn =
   names(df1) <- c("Estimate",  "Std. Error")
 
   return (list(bayesian = df1, summary = p2, raw = p1 ))
+}
+
+
+## Bayesian linear regression for binary outcome (i.e., Linear Probability Model )   -------------
+
+#' Bayesian estimator for linear regression with binary classification as *dependent* variable
+#'
+#' This functions takes binary classification as *dependent* variables for linear probability  model, apply Bayesian estimator, and returned corrected coefficient estimates.
+#'
+#' @param formula R formula, such as y ~ x
+#' @param data a data.frame object
+#' @param fnr false negative rate
+#' @param fpr false positive rate
+#' @param fp number of false positives in validation data
+#' @param fn number of false negatives in validation data
+#' @param tp number of true positives in validation data
+#' @param tn number of true negatives in validation data
+#' @param tau_prior prior values of hyperparameters of Bayesian regression variance
+#' @param working_dir directory to store JAGS temporary files; default is the same directory of the script
+#' @param n.iter number of Bayesian draws
+#' @return a dataframe contining estimated values, standard errors, z values and p values
+#' @export
+bayesian_LPM <- function(formula, data, fn = fn, fp = fp, tp = tp, tn = tn, fpr = fpr, fnr = fnr, tau_prior = 0.001, working_dir = "", n.iter = n.iter){
+
+  cmodel <- function(){
+
+    ## new setup
+    for( i in 1 : N) {
+      y[i] ~ dbern(q[i])
+
+      q[i] <- w[i] * (1-lambda[1] - lambda[2]) + lambda[2]
+
+      w[i] <- ifelse(u[i]>1, 0.99999, u[i])
+      u[i] <- ifelse(p[i]<0, 0.00001, p[i])
+
+      p[i] <- inprod(x[i,], beta[])
+    }
+
+    # standard logistic
+    for( l in 1:num_param){
+      beta[l] ~ dnorm(0, tau)
+    }
+
+    tau ~ dgamma(tau_prior, tau_prior)
+
+    # fpr/fnr estimate
+    lambda[1] ~ dbeta(fn , tp ) ## FNR
+    lambda[2] ~ dbeta(fp , tn ) ## FPR
+
+  }
+
+
+  ## get an initial model (to obtain data matrix)
+  ps = na.omit(data)
+  initModel = lm(formula, data = ps)
+  mX = as.matrix(model.matrix(initModel))
+  vY = model.frame(initModel)[,1]
+
+  MLE <- MLE_LPM(formula, ps, fnr = fnr, fpr = fpr )
+
+  vBetfnr = MLE[,1]
+
+  dat <- list(y = vY, x = mX, fn = fn, fp = fp, tp = tp, tn = tn, N = nrow(ps), num_param = ncol(mX), tau_prior = tau_prior)
+
+
+  # parameter initialization
+  param_inits <- function(){
+    list(lambda=c(fnr, fpr), beta = vBetfnr)
+  }
+
+  param <- c("lambda",  "beta")
+
+  ## to use autojags one has to have at least 2 chains
+  # otherwise the Gelman-Rubin statistics cannot be calculated
+  out1 <- jags(data = dat, inits = param_inits, parameters.to.save = param, model.file = cmodel, n.chains = 2, n.burnin = 200,  n.iter = 400,  working.directory = working_dir)
+  out <- autojags(out1, n.iter = n.iter)
+
+  p1 <- as.mcmc(out)
+  p2 <- summary(p1)
+
+  df1 = data.frame(p2$statistics[, c(1,2)])
+  names(df1) <- c("Estimate",  "Std. Error")
+
+  return (list(bayesian = df1, summary = p2, raw = p1 ))
+}
+
+
+
+## validation from error rates
+#' Produce a list of pseudo-true labels / predictions
+#' used for Bootstrap estimator, if only FNR/FPR are known
+
+#'
+#' @param n Size of intended validation dataset
+#' @param mu_pred mean of positive class in the predicted labels
+#' @param fnr false negative rate
+#' @param fpr false positive rate
+#' @return a list, the first element is a vector containing true labels, the second element is a vector containing predicted labels.
+#' @export
+produce_valdata_from_errorrate <- function(n, mu_pred, fpr, fnr)
+{
+
+  mu <- (mu_pred - fpr) / ( 1-fpr-fnr)
+  num1 <- round (n*mu) ## number of positive in truth
+  num0 <- n - num1 ## number of negative in truth
+
+  tl1 <- rep(1, num1)
+  tl0<- rep(0, n - num1)
+
+  ## then flip labels based on FNR/FPR
+  fpc <- round (num0 * fpr)
+  fnc <- round (num1 * fnr)
+
+  pl0 <- c(rep(1, fpc), rep(0, num0 - fpc))
+  pl1 <- c(rep(0, fnc), rep(1, num1 - fnc))
+
+  return (list(tl =c(tl1, tl0), pl = c(pl1, pl0) ))
+
 }
